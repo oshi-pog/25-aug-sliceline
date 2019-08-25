ab39f1f8a259f6a7cbdba31b29837a31e6e07af5
josh-kea <josh0661@stud.kea.dk>
2019-08-25 21:13:20 +0200

    global styling done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+//Defining the global style
+const GlobalStyle = createGlobalStyle`
+  body {
+    
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 function App() {
-  return <div>Hello Sliceline</div>;
+  return (
+  <>
+  <h1>Sliceline</h1>
+  <GlobalStyle/><div>Hello Sliceline</div>
+  </>
+  );
 }
 
 export default App;
